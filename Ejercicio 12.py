f96069e83d9182e8ab898849487ecea64d6d0adb
Makkouh2 <145025546+Makkouh2@users.noreply.github.com>
2023-10-05 19:22:57 +0200

    Ejercicio 12

---
+++ Ejercicio 12.py
@@ -0,0 +1,7 @@
+x= int(input("Numero de barras vendidas no frescas"))
+panfresco= 3.49
+descuento= 0.6
+Pan_no_fresco= x*panfresco*(1-descuento)
+print("Precio barra fresca" ,panfresco)
+print("Descuento" ,descuento)
+print("Total barras no frescas" ,Pan_no_fresco)
